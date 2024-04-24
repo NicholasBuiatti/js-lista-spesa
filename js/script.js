3793267c06a8b31b81676216898b3b86e4c76301
NicholasBuiatti <nic.buiatti@gmail.com>
2024-04-24 16:06:30 +0200

    m-2 creo il ciclo while m-3 pusho con innerHTML

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 //creazione array
 const list = ['latte', 'uova', 'pane', 'pomodori', 'insalata', 'marmellata'];
+console.log(list);
 
 //creazione ciclo for
-for (let i = 0; i < list.length; i++) {
-    //salvo l'elemento per ogni incremento in una costante
-    const item = list[i];
-    console.log(i, item);
+// for (let i = 0; i < list.length; i++) {
+//     //salvo l'elemento per ogni incremento in una costante
+//     const item = list[i];
+//     console.log(i, item);
+// }
+
+let myUl = document.querySelector('ul');
+
+//creazione ciclo while
+//creazione variabile contatore
+let counter = 0
+//imposto la condizione fino a quando fuziona
+while (counter < list.length) {
+    //salvo in una variabile l'elemento
+    const item = list[counter];
+    console.log(counter, item);
+
+    myUl.innerHTML += `
+    <li class='text-warning'>
+        ${item}
+    </li>    
+    `
+    //incremento il contatore
+    counter++;
 }
+//resetto il contatore (per un futuro)
+counter = 0;
